f490793aa60968145b11505a3b8930fee475d287
Anıl Şenay <anilsenay3@gmail.com>
2018-12-20 00:54:34 +0300

    modules added.

--- sinefil.py
+++ sinefil.py
@@ -2,8 +2,8 @@ import random
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://www.imdb.com/chart/top")
-soup = BeautifulSoup(r.content, "html.parser")
+requestAdress = requests.get("https://www.imdb.com/chart/top")
+soup = BeautifulSoup(requestAdress.content, "html.parser")
 
 films = soup.find_all("td", attrs = {"class":"titleColumn"})
 rates = soup.find_all("td", attrs = {"class":"ratingColumn"})
@@ -13,11 +13,11 @@ print("We suggest you to watch: " + films[random].a.text + " (rate:" + rates[ran
 choice = input("Do you want to see IMDB Top 100 list? (Y/N): ")
 
 if(choice == 'Y' or choice == 'y'):
-	i = 0
+	index = 0
 	rateIndex = 0
-	while (i < 100):
+	while (index < 100):
 		#print(films[i].a.text, "(IMDB rate: " + rates[rateIndex].strong.text + ")")
-		print((i+1),")",'{:71}'.format(films[i].a.text), '{:>10}'.format("(rate:"+rates[rateIndex].strong.text+")"))
-		i = i + 1
+		print((index+1),")",'{:75}'.format(films[index].a.text), '{:>10}'.format("(rate:"+rates[rateIndex].strong.text+")"))
+		index = index + 1
 		rateIndex = rateIndex + 2
 
